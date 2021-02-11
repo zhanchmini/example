a0ae7e48db1f5ccdbbda30b3358b96f2d9885175
zhanch <761324952@qq,com>
2021-02-11 20:09:56 +0800

    example

---
+++ src/common/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace zhanchmini\example\common;
+
+class Index
+{
+    /**
+     * @return array
+     */
+    public function run()
+    {
+        return [
+            'Hello World!'
+        ];
+    }
+}
