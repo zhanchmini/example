dc49fb6b514539d2d3a8954b8bdd8b0dbb2e2439
zhanch <761324952@qq,com>
2021-02-13 15:40:28 +0800

    update

--- src/yii2/migrations/m210211_113705_create_example_table.php
+++ src/yii2/migrations/m210211_113705_create_example_table.php
@@ -13,9 +13,11 @@ class m210211_113705_create_example_table extends Migration
     public function safeUp()
     {
         $this->createTable('{{%example}}', [
-            'id' => $this->primaryKey(),
+            'id' => $this->primaryKey()->unsigned()->comment('ID'),
             'title' => $this->string()->notNull()->defaultValue('')->comment('标题'),
             'content' => $this->text()->comment('内容'),
+            'created_at' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('创建时间'),
+            'updated_at' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('更新时间'),
         ]);
     }
 
--- src/yii2/models/Example.php
+++ src/yii2/models/Example.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\models;
+namespace zhanchmini\example\yii2\models;
 
 use Yii;
 
